e666f3e19ffa93741835190fa225831b1c3b300d
Ndrit2012 <ndritindriti38@gmail.com>
2026-09-07 16:11:02 +0200

    7/9/2026
    3:53pm

---
+++ modul2/index.php
@@ -0,0 +1,32 @@
+<?php
+$school="Kreme Kreme";
+echo "I love $school";
+
+$x=120;
+$y=50;
+
+echo "<br>";
+echo $x + $y;
+echo "<br>";
+echo $x - $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x % $y;
+echo "<br>";
+
+$a="Gipi";
+$b="Boy";
+$c=$a.$b;
+echo $c;
+
+$the_string="CuliGipiPomuchKremeKremeOG";
+echo "<br>";
+echo strlen($the_string);
+echo "<br>";
+echo str_word_count($the_string);
+echo "<br>";
+echo strrev($the_string);
+?>
