f50f4fe918720c4734bf42253d21700c4dc78faf
ensarpacolli <ensarpacolli011@outlook.com>
2026-09-21 16:17:02 +0200

    21/9/2026
    3:34pm

--- modul4/index.php
+++ modul4/index.php
@@ -74,4 +74,48 @@ function callCounter(){
 }
 callCounter(); //count=1
 callCounter();
+echo "<br>";
+
+// $basketball_players=array("LeBron James,Steph Curry,Michael Jordan,Kobe Bryant,Anthony Edwards"); //I
+$basketball_players=["LeBron James","Steph Curry","Michael Jordan","Kobe Bryant","Anthony Edwards"];         //II
+
+echo $basketball_players[0];
+echo "<br>";
+echo end($basketball_players);
+echo "<br>";
+echo count($basketball_players);
+echo "<br>";
+echo "<br>";
+
+array_push($basketball_players,"Jalen Brunson");
+array_unshift($basketball_players, "Victor Wembanyama");
+
+echo "<br>";
+echo count($basketball_players);
+for($i=0;$i<7;$i++){
+    echo $basketball_players[$i];
+    echo "<br>";
+}
+
+
+array_shift($basketball_players); // - removes the first item
+array_pop($basketball_players);  //- removes the last item
+
+echo "<br>";
+echo "<br>";
+
+for($i=0;$i<5;$i++){
+    echo $basketball_players[$i];
+    echo "<br>";
+}
+echo "<br>";
+echo "<br>";
+var_dump($basketball_players);
+
+$output1=array_slice($basketball_players,4);
+$output2=array_slice($basketball_players,0,3);
+echo "<br>";
+var_dump($output1);
+echo "<br>";
+var_dump($output2);
 ?>
